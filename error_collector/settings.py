08336d40978b41104aac6819dfc36b512ad41f93
Peter <peter@pbwis.com>
2021-04-15 20:23:14 +0100

    read csv

--- error_collector/settings.py
+++ error_collector/settings.py
@@ -110,4 +110,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'data'),
     )
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from io import StringIO
 from django.shortcuts import render
 from .models import Post
+import csv, os
 #from django.views.generic import ListView
 
 
@@ -8,3 +9,11 @@ from .models import Post
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'posts/posts_list.html', {'posts': posts})
+
+
+def read_csv(request):
+    current_path = os.path.dirname(__file__)
+    data_folder = os.path.join(current_path, data)
+    os.path.join(data_folder, software.csv)
+    file = file_path.open(mode="r", encoding="utf-8", newline=" ")
+    pass
