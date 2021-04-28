61643456debf915bf24271db76b3a5011d24f831
Peter <peter@pbwis.com>
2021-04-28 22:55:43 +0100

    read_delete

--- posts/views.py
+++ posts/views.py
@@ -14,13 +14,6 @@ def posts_list(request):
     return render(request, 'posts/posts_list.html', {'posts': posts})
 
 
-def read_csv(request):
-    current_path = os.path.dirname(__file__)
-    data_folder = os.path.join(current_path, data)
-    os.path.join(data_folder, software.csv)
-    file = file_path.open(mode="r", encoding="utf-8", newline=" ")
-    pass
-
 
 def upload_file(request):
     form = CsvModelForm(request.POST or None, request.FILES or None)
