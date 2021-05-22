fc129e1a653fd950cee788c3072acf2c400045b4
Peter <peter@pbwis.com>
2021-05-22 20:21:43 +0100

    get adapter details

---
+++ posts/check_adapter.ps1
@@ -0,0 +1,8 @@
+# Part of project - get adapter details
+
+$IPType = 'IPv4'
+$Adapter = Get-NetAdapter | Where-Object Status -EQ 'Up'
+$Interface = $Adapter | Get-NetIPInterface -AddressFamily $IPType
+$Index = $Interface.ifIndex
+Get-NetIPAddress -InterfaceIndex $Index -AddressFamily $IPType |
+Format-Table -Property Interface*, IPAddress, PrefixLength
