0cf0be731b99a3c1b935a8849031a2e48429232e
Peter <peter@pbwis.com>
2021-06-07 20:19:43 +0100

    upload_view

--- csvs/views.py
+++ csvs/views.py
@@ -5,4 +5,7 @@ from .forms import CsvModelForm
 
 def upload_file_view(request):
     form = CsvModelForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        form = CsvModelForm()
     return render(request, 'csvs/upload.html', {'form': form})
