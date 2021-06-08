9fb327abb0b66cae95a43bd787e46a702576930b
Peter <peter@pbwis.com>
2021-06-08 20:54:28 +0100

    update view

--- csvs/urls.py
+++ csvs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import upload_file_view
+from .views import upload_file_view, list_of_data
 
 
 app_name = 'csvs'
 
 urlpatterns = [
     path('', upload_file_view, name='upload-view'),
+    path('list', list_of_data, name='list' ),
 ]
--- csvs/views.py
+++ csvs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import CsvModelForm
+from .models import Csv
+import csv
 #from django.http import HttpResponse
 
 
@@ -8,4 +10,20 @@ def upload_file_view(request):
     if form.is_valid():
         form.save()
         form = CsvModelForm()
+        obj = Csv.objects.get(activated=False)
+        with open(obj.file_name.path, 'r') as f:
+            reader = csv.reader(f)
+            for i, row in enumerate(reader):
+                if i == 0:
+                    pass
+                else:
+                    row = "".join(row)
+                    row = row.replace(";", " ")
+                    print(row)
+            obj.activated = True
+            obj.save()
     return render(request, 'csvs/upload.html', {'form': form})
+
+
+def list_of_data(request):
+    return render(request, 'csvs/list.html')
